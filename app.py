5b4c8c5a49936ddf3b677bdea9a87b779012713e
Michael Krueger <michael.krueger@europace.de>
2019-07-14 12:31:56 +0200

    Prozessverhalten unterschieden nach install/usage/fehler

--- app.py
+++ app.py
@@ -9,35 +9,46 @@ def install():
     return render_template('install.html')
 
 @app.route('/grant')
-def code():
-    # Access-Token holen
+def setState():
+
     code = request.args['code']
-    url = 'https://slack.com/api/oauth.access?client_id=14917766709.693327534246&client_secret=f844dde28cb3bad0f7f2b11f160455c7&code='+ code + '&redirect_uri=https://slack-auth.herokuapp.com/grant'
-    r = requests.get(url)
-    rjson = json.loads(r.text)
-    accessToken = rjson['access_token']
-    # Todo: Access-Token speichern
+    if(code > ""):
+        # Access-Token holen
+        url = 'https://slack.com/api/oauth.access?client_id=14917766709.693327534246&client_secret=f844dde28cb3bad0f7f2b11f160455c7&code='+ code + '&redirect_uri=https://slack-auth.herokuapp.com/grant'
+        r = requests.get(url)
+        rjson = json.loads(r.text)
+        accessToken = rjson['access_token']
+        # Todo: Access-Token speichern
 
-    # User identifizieren
-    header = headers = {
-        'Authorization': 'Bearer ' + accessToken
-    }
-    url = 'https://slack.com/api/auth.test'
-    r = requests.get(url, headers=header)
-    user = json.loads(r.text)['user_id']
+    userId = request.args['userid']
+    if(userId > ""):
+        # Access-Token auslesen
+        return "userId=" + userId
 
-    # User-Profile Status setzen
-    header = headers = {
-            'Authorization': 'Bearer '+accessToken,
-            'Content-type' : 'application/json; charset=utf-8'
+    if(accessToken > ""):
+        # User identifizieren
+        header = headers = {
+            'Authorization': 'Bearer ' + accessToken
         }
-    url = 'https://slack.com/api/users.profile.set'
-    data = {
-        "profile": {
-            "status_text": "riding a train",
-            "status_emoji": ":mountain_railway:"
-        }
-    }
-    r = requests.post(url = url, data = json.dumps(data), headers = header)
+        url = 'https://slack.com/api/auth.test'
+        r = requests.get(url, headers=header)
+        user = json.loads(r.text)['user_id']
 
-    return "Ready..."
+
+
+
+        # User-Profile Status setzen
+        header = headers = {
+                'Authorization': 'Bearer '+accessToken,
+                'Content-type' : 'application/json; charset=utf-8'
+            }
+        url = 'https://slack.com/api/users.profile.set'
+        data = {
+            "profile": {
+                "status_text": "riding a train",
+                "status_emoji": ":mountain_railway:"
+            }
+        }
+        r = requests.post(url = url, data = json.dumps(data), headers = header)
+    else:
+        return render_template('install.html')
