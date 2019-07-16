c0bcd6970787fdfe4d4158716f0115839ead9c2e
Michael Krueger <michael.krueger@europace.de>
2019-07-16 07:21:29 +0200

    Access Token speichern

--- app.py
+++ app.py
@@ -69,6 +69,6 @@ def grant():
             }
         }
         r = requests.post(url = url, data = json.dumps(data), headers = header)
-        return render_template('install_ready.html')
+        return render_template('install_ready.html', userId=userId)
     else:
         return render_template('install.html')
